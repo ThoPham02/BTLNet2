535fc340bc8e4e33a32789c5f42a37c254f7601f
Pham Ba Tho <thopb@vivas.vn>
2023-12-13 13:14:31 +0700

    tmp

--- Areas/Identity/Controllers/AccountController.cs
+++ Areas/Identity/Controllers/AccountController.cs
@@ -22,12 +22,26 @@ namespace HotelManagement.Areas.Identity.Controllers
 {
     [Authorize]
     [Area("Identity")]
-    [Route("Account/[action]")]
+    [Route("/Account/[action]")]
     public class AccountController : Controller
     {
-        // ... (existing code)
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
+        public string StatusMessage { get; set;}
 
-        [HttpGet("Login")]
+        public AccountController(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        // GET: /Account/Login
+        [HttpGet("/login/")]
         [AllowAnonymous]
         public IActionResult Login(string returnUrl = null)
         {
@@ -35,18 +49,19 @@ namespace HotelManagement.Areas.Identity.Controllers
             return View();
         }
 
-        [HttpPost("Login")]
+        //
+        // POST: /Account/Login
+        [HttpPost("/login/")]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
             ViewData["ReturnUrl"] = returnUrl;
-
             if (ModelState.IsValid)
             {
                 var result = await _signInManager.PasswordSignInAsync(model.UserNameOrEmail, model.Password, model.RememberMe, lockoutOnFailure: true);
-
+                // Tìm UserName theo Email, đăng nhập lại
                 if ((!result.Succeeded) && HotelManagementUtilities.IsValidEmail(model.UserNameOrEmail))
                 {
                     var user = await _userManager.FindByEmailAsync(model.UserNameOrEmail);
@@ -63,14 +78,16 @@ namespace HotelManagement.Areas.Identity.Controllers
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Không đăng nhập được.");
+                    ModelState.AddModelError("Không đăng nhập được.");
                 }
+            } else {
+                ModelState.AddModelError("Không đăng nhập được.");
             }
-
             return View(model);
         }
 
-        [HttpPost("LogOff")]
+        // POST: /Account/LogOff
+        [HttpPost("/logout/")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LogOff()
         {
@@ -78,16 +95,19 @@ namespace HotelManagement.Areas.Identity.Controllers
             _logger.LogInformation("User đăng xuất");
             return RedirectToAction("Index", "Home", new { area = "" });
         }
-
-        [HttpGet("Register")]
+        //
+        // GET: /Account/Register
+        [HttpGet("/register/")]
         [AllowAnonymous]
         public IActionResult Register(string returnUrl = null)
         {
+            returnUrl ??= Url.Content("~/");
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
-
-        [HttpPost("Register")]
+        //
+        // POST: /Account/Register
+        [HttpPost("/register/")]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
@@ -98,10 +118,9 @@ namespace HotelManagement.Areas.Identity.Controllers
             // if (ModelState.IsValid)
             // {
             //     var user = await _userManager.FindByEmailAsync(model.Email);
-
             //     if (user == null)
             //     {
-            //         user = new User
+            //         user = new User()
             //         {
             //             FullName = "",
             //             Address = "",
@@ -115,10 +134,37 @@ namespace HotelManagement.Areas.Identity.Controllers
             //         await _signInManager.SignInAsync(user, false);
 
             //         return RedirectToAction("Index");
+
             //     }
+            //     else
+            //     {
+
+            //     }
+
+            //     StatusMessage = "Tạo Admin Account thành công!!";
+            //     return RedirectToAction("Index");
             // }
 
-            // return View(model);
+            return View(model);
+        }
+
+        //
+        // GET: /Account/ForgotPassword
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/ForgotPassword
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            return View(model);
         }
     }
 }
